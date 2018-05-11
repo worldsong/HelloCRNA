a11a304bd6d3c2f65b15da66bf822f94d547dd44
worldsong <2060884089@qq.com>
2018-05-11 20:58:23 +0800

    闪烁的文字 props vs state。

--- App.js
+++ App.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-class Greeting extends Component {
+class Blink extends Component {
+    constructor(props){
+        super(props);
+        this.state = { showText: true };
+
+        setInterval(()=> {
+            this.setState(previousState => {
+                return { showText: !previousState.showText };
+            });
+        }, 1000);
+    }
     render(){
-        return (
-            <Text>Hello {this.props.name }!</Text>
+        let display = this.state.showText ? this.props.text : ' ';
+        return(
+            <Text>{display}</Text>
         )
     }
 }
 
-export default class LotsOfGreetings extends Component{
-    render(){
+export default class BlinkApp extends Component {
+    render() {
         return (
-            <View style={{alignItems: 'center'}}>
-                <Greeting name='Song'></Greeting>
-                <Greeting name='Song'></Greeting>
-                <Greeting name='Song'></Greeting>
+            <View>
+                <Blink text='I love to blink' />
+                <Blink text='Yes blinking is so great' />
+                <Blink text='Why did they ever take this out of HTML' />
+                <Blink text='Look at me look at me look at me' />
             </View>
-        )
+        );
     }
 }
