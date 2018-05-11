3d51e16e0e0ae1d5b83eecc13c2e3e1efb5db3f2
worldsong <2060884089@qq.com>
2018-05-11 22:09:48 +0800

    在Text内部的元素不再使用flexbox布局，而是采用文本布局。

--- App.js
+++ App.js
@@ -1,40 +1,13 @@
 import React, { Component } from 'react';
 import { Text, StyleSheet } from 'react-native';
 
-export default class TextInANest extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            titleText: "Bird's Nest",
-            bodyText: 'This is not really a bird nest.'
-        };
-    }
-
-    onPressTitle(){
-        // alert('翻牌了！')
-        console.log('翻牌了')
-    }
-
+export default class TextApp extends Component {
     render() {
         return (
-            <Text style={styles.baseText}>
-                <Text style={styles.titleText}>
-                    {this.state.titleText}{'\n'}{'\n'}
-                </Text>
-                <Text numberOfLines={5} onPress={this.onPressTitle}>
-                    {this.state.bodyText}
-                </Text>
+            <Text>
+                <Text>First part and </Text>
+                <Text>second part</Text>
             </Text>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    baseText: {
-        fontFamily: 'serif',
-    },
-    titleText: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-});
