e6aad6f4b6c3d881ada28f9bea495f99f8901268
worldsong <2060884089@qq.com>
2018-05-11 22:27:55 +0800

    Fetch数据交互及远程调试控制台数据打印。

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
-import { SectionList, StyleSheet, Text, View } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
 
-export default class SectionListBasics extends Component {
+var BASE_URL = 'https://api.github.com/repos/facebook/react-native/events';
+
+export default class FetchDemo extends Component {
+
+    getEvent() {
+        // 取得 BASE_URL response 的資料
+        fetch(BASE_URL)
+            .then((res) => res.json())
+            .then((responseData) => {
+                // 單純只做 log
+                console.log(responseData);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            .done();
+    }
+
     render() {
+        // getEvent 只會 log 出得到的 data, 不會有任何 render
+        this.getEvent();
+
         return (
             <View style={styles.container}>
-                <SectionList
-                    sections={[
-                        {title: '球类', data: ['篮球', '足球' , '乒乓球']},
-                        {title: '水果', data: ['苹果', '香蕉', '橘子', '榴莲', '西瓜']},
-                        {title: '主食', data: ['面包', '馒头', '米饭']}
-                    ]}
-                    renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-                    renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-                />
+                <Text>
+                    Welcome to React Native!
+                </Text>
             </View>
         );
     }
@@ -22,20 +40,9 @@ export default class SectionListBasics extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 22
-    },
-    sectionHeader: {
-        paddingTop: 2,
-        paddingLeft: 10,
-        paddingRight: 10,
-        paddingBottom: 2,
-        fontSize: 18,
-        fontWeight: 'bold',
-        backgroundColor: 'rgba(247,247,247,1.0)',
-    },
-    item: {
-        padding: 10,
-        fontSize: 16,
-        height: 44,
-    },
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    }
 })
