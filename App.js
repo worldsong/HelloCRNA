39d273ca85c934436718cbd42bf881e1ab3d86a8
worldsong <2060884089@qq.com>
2018-05-11 22:22:07 +0800

    FlatList列表组件。

--- App.js
+++ App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import { Button  } from 'react-native';
+import { AppRegistry, FlatList, StyleSheet, Text, View } from 'react-native';
 
-export default class App extends Component {
-    onPressLearnMore(){
-        alert('Learn more about this purple button')
-    }
+export default class FlatListBasics extends Component {
     render() {
         return (
-            <Button
-                onPress={this.onPressLearnMore}
-                title="Learn More"
-                color="#841584"
-                accessibilityLabel="Learn more about this purple button"
-            />
+            <View style={styles.container}>
+                <FlatList
+                    data={[
+                        {key: 'Devin'},
+                        {key: 'Jackson'},
+                        {key: 'James'},
+                        {key: 'Joel'},
+                        {key: 'John'},
+                        {key: 'Jillian'},
+                        {key: 'Jimmy'},
+                        {key: 'Julie'},
+                    ]}
+                    renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+                />
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 22
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
+})
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => FlatListBasics);
