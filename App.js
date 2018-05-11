7751baba69c4e54cf82099e0989dfd29ddb90169
worldsong <2060884089@qq.com>
2018-05-11 22:24:36 +0800

    SectionList渲染分组数据。

--- App.js
+++ App.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
-import { AppRegistry, FlatList, StyleSheet, Text, View } from 'react-native';
+import { SectionList, StyleSheet, Text, View } from 'react-native';
 
-export default class FlatListBasics extends Component {
+export default class SectionListBasics extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <FlatList
-                    data={[
-                        {key: 'Devin'},
-                        {key: 'Jackson'},
-                        {key: 'James'},
-                        {key: 'Joel'},
-                        {key: 'John'},
-                        {key: 'Jillian'},
-                        {key: 'Jimmy'},
-                        {key: 'Julie'},
+                <SectionList
+                    sections={[
+                        {title: '球类', data: ['篮球', '足球' , '乒乓球']},
+                        {title: '水果', data: ['苹果', '香蕉', '橘子', '榴莲', '西瓜']},
+                        {title: '主食', data: ['面包', '馒头', '米饭']}
                     ]}
-                    renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+                    renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+                    renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
                 />
             </View>
         );
@@ -28,12 +24,18 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: 22
     },
+    sectionHeader: {
+        paddingTop: 2,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 2,
+        fontSize: 18,
+        fontWeight: 'bold',
+        backgroundColor: 'rgba(247,247,247,1.0)',
+    },
     item: {
         padding: 10,
-        fontSize: 18,
+        fontSize: 16,
         height: 44,
     },
 })
-
-// skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => FlatListBasics);
