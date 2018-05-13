6678a4bf3d311db77aa7780eafe5675124b758cb
worldsong <2060884089@qq.com>
2018-05-13 16:27:56 +0800

    ScrollView 滚动视图组件。

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import React, {Component} from 'react';
 
 const BagView = require('./components/BagView');
 const LoginView = require('./components/LoginView');
+const MyScrollView = require('./components/MyScrollView');
 
 export default class first extends Component {
     render() {
         /*
                 return <BagView />
         */
+/*
         return <LoginView />
+*/
+        return <MyScrollView />
     }
 }
