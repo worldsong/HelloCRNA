96ab0e3778e904b1d665e15881c05496a0df9eed
worldsong <2060884089@qq.com>
2018-05-15 22:00:08 +0800

    ScrollView:滚动视图组件。

---
+++ components/MyScrollView.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    ScrollView
+} from 'react-native';
+
+const Dimensions = require('Dimensions');
+const ScreenWidth = Dimensions.get('window').width;
+
+class MyScrollView extends Component {
+    renderChild = () => {
+        const colors = ['red','green','blue','yellow'];
+        return colors.map((item,i)=>{
+            return <View key={`key${i}`} style={{backgroundColor:item,width:ScreenWidth,height:200}}>
+                <Text>{i}</Text>
+            </View>;
+        });
+    }
+    render(){
+        return (
+            <ScrollView
+                horizontal={true}
+                pagingEnabled={true}
+                showsHorizontalScrollIndicator={false}
+            >
+                {this.renderChild()}
+            </ScrollView>
+        )
+    }
+}
+
+module.exports = MyScrollView;
