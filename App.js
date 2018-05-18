47d77444724673a93592fb989de09f5f594beec4
worldsong <2060884089@qq.com>
2018-05-18 21:02:35 +0800

    自定义底部导航。

--- App.js
+++ App.js
@@ -1,25 +1,53 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { Text, View } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createBottomTabNavigator } from 'react-navigation';
 
-const BagView = require('./components/BagView');
-const LoginView = require('./components/LoginView');
-const MyScrollView = require('./components/MyScrollView');
-const BannerView = require('./components/BannerView');
-const MyListView = require('./components/MyListView');
-
-export default class first extends Component {
+class HomeScreen extends React.Component {
     render() {
-        /*
-                return <BagView />
-        */
-/*
-        return <LoginView />
-*/
-/*
-        return <MyScrollView />
-*/
-/*
-        return <BannerView />
-*/
-        return <MyListView />
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Home!</Text>
+            </View>
+        );
     }
 }
+
+class SettingsScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Settings!</Text>
+            </View>
+        );
+    }
+}
+export default createBottomTabNavigator(
+    {
+        Home: HomeScreen,
+        Settings: SettingsScreen,
+    },
+    {
+        navigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, tintColor }) => {
+                const { routeName } = navigation.state;
+                let iconName;
+                if (routeName === 'Home') {
+                    iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+                } else if (routeName === 'Settings') {
+                    iconName = `ios-options${focused ? '' : '-outline'}`;
+                }
+
+                // You can return any component that you like here! We usually use an
+                // icon component from react-native-vector-icons
+                return <Ionicons name={iconName} size={25} color={tintColor} />;
+            },
+        }),
+        tabBarOptions: {
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+        },
+        animationEnabled: false,
+        swipeEnabled: false,
+    }
+);
