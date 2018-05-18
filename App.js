ed1faff3d261b5cd41ff3624f96f55bd06f27992
worldsong <2060884089@qq.com>
2018-05-18 21:51:09 +0800

    TabNavigator 已过时警告。不再使用！

--- App.js
+++ App.js
@@ -1,53 +1,33 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { createBottomTabNavigator } from 'react-navigation';
+import { View, Text,Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import { TabNavigator } from 'react-navigation';
 
-class HomeScreen extends React.Component {
-    render() {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Home!</Text>
-            </View>
-        );
-    }
-}
-
-class SettingsScreen extends React.Component {
-    render() {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Settings!</Text>
-            </View>
-        );
-    }
-}
-export default createBottomTabNavigator(
-    {
-        Home: HomeScreen,
-        Settings: SettingsScreen,
-    },
-    {
-        navigationOptions: ({ navigation }) => ({
-            tabBarIcon: ({ focused, tintColor }) => {
-                const { routeName } = navigation.state;
-                let iconName;
-                if (routeName === 'Home') {
-                    iconName = `ios-information-circle${focused ? '' : '-outline'}`;
-                } else if (routeName === 'Settings') {
-                    iconName = `ios-options${focused ? '' : '-outline'}`;
-                }
-
-                // You can return any component that you like here! We usually use an
-                // icon component from react-native-vector-icons
-                return <Ionicons name={iconName} size={25} color={tintColor} />;
-            },
-        }),
-        tabBarOptions: {
-            activeTintColor: 'tomato',
-            inactiveTintColor: 'gray',
-        },
-        animationEnabled: false,
-        swipeEnabled: false,
-    }
+const HomeScreen = () => (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+    </View>
 );
+
+const ProfileScreen = () => (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Profile Screen</Text>
+    </View>
+);
+
+const RootTabs = TabNavigator({
+    Home: {
+        screen: HomeScreen,
+    },
+    Profile: {
+        screen: ProfileScreen,
+    },
+},{
+    tabBarPosition: 'bottom',
+    animationEnabled: true,
+    tabBarOptions: {
+        activeTintColor: '#ffffff',
+    }
+});
+
+export default RootTabs;
