807f1206ff739d51a73951c099f8fea63c8c6e26
worldsong <2060884089@qq.com>
2018-05-18 22:07:46 +0800

    最直白的堆栈导航器，经路由控制的一个页面。

--- App.js
+++ App.js
@@ -1,35 +1,19 @@
 import React from 'react';
-import { View, Text,Button } from 'react-native';
-import { createMaterialTopTabNavigator } from 'react-navigation';
+import {View, Text} from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 
-// TabNavigator is deprecated.
-// Please use the createBottomTabNavigator
-// or createMaterialTopTabNavigator instead.
-
-const HomeScreen = () => (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-    </View>
-);
-
-const ProfileScreen = () => (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Profile Screen</Text>
-    </View>
-);
-
-const RootTabs = createMaterialTopTabNavigator({
-    Home: {
-        screen: HomeScreen,
-    },
-    Profile: {
-        screen: ProfileScreen,
-    },
-},{
-    tabBarOptions: {
-        activeTintColor: 'tomato',
-        inactiveTintColor: 'gray',
+class HomeScreen extends React.Component {
+    render(){
+        return (
+            <View style={{flex: 1, alignItems: 'center', justifyContent:'center'}}>
+                <Text>Home Screen</Text>
+            </View>
+        )
     }
-});
+}
 
-export default RootTabs;
+export default createStackNavigator({
+    Home: {
+        screen: HomeScreen
+    }
+})
