dd9755fac063125e7116fad53680e0671c9aca35
worldsong <2060884089@qq.com>
2018-05-19 14:40:58 +0800

    作业：创建底部选项卡布局。

--- App.js
+++ App.js
@@ -1,50 +1,32 @@
 import React from 'react';
-import { Button, View, Text} from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { View, Text,Button } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation';
 
-class HomeScreen extends React.Component {
-    render(){
-        return (
-            <View style={{flex: 1, alignItems: 'center', justifyContent:'center'}}>
-                <Text>Home Screen</Text>
-                <Button
-                    title="跳转到详情页"
-                    onPress={()=> this.props.navigation.navigate('Details')}
-                />
-            </View>
-        )
-    }
-}
+const HomeScreen = () => (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+    </View>
+);
 
-class DetailsScreen extends React.Component {
-    render() {
-        return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Details Screen</Text>
-                <Button
-                    title="跳转到Home页"
-                    onPress={()=> this.props.navigation.navigate('Home')}
-                />
-                <Button
-                    title="再一次跳转到Details页"
-                    onPress={()=> {console.log(this.props.navigation);this.props.navigation.push('Details')}}
-                />
-            </View>
-        );
-    }
-}
+const ProfileScreen = () => (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Profile Screen</Text>
+    </View>
+);
 
-const RootStack = createStackNavigator(
-    {
-        Home: HomeScreen,
-        Details: DetailsScreen
+const RootTabs = createBottomTabNavigator({
+    Home: {
+        screen: HomeScreen,
     },
-    {
-        initialRouteName: 'Details'
-    });
-
-export default class App extends React.Component {
-    render(){
-        return <RootStack />
+    Profile: {
+        screen: ProfileScreen,
+    },
+},{
+    tabBarPosition: 'bottom',
+    animationEnabled: true,
+    tabBarOptions: {
+        activeTintColor: '#ffffff',
     }
-}
+});
+
+export default RootTabs;
