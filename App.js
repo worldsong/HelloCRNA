0929ff7d228294e81848c566cf1a242f217f028a
worldsong <2060884089@qq.com>
2018-05-19 15:22:17 +0800

    作业：实现我的组件布局。

--- App.js
+++ App.js
@@ -88,8 +88,47 @@ const FindScreen = () => (
 );
 //我的组件
 const MyScreen = () => (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Profile Screen</Text>
+    <View style={[styles.container,styles.findBody]}>
+        <HeadScreen title="我的"></HeadScreen>
+        <View style={styles.findView}>
+            <Icon style={[styles.findViewIcon,{flex:2}]} name="ios-contacts-outline" color="#3399FF" size={60} />
+            <View style={[styles.findViewText,{flex:8,justifyContent:'center'}]}>
+                <Text>worldsong</Text>
+                <Text style={{color:'gray'}}>微信号：songfens.club</Text>
+            </View>
+
+        </View>
+
+        <View style={styles.findView}>
+            <Icon style={styles.findViewIcon} name="ios-cash-outline" color="#3399FF" size={22} />
+            <Text style={styles.findViewText}>钱包</Text>
+        </View>
+
+        <View style={styles.findView}>
+            <Icon style={styles.findViewIcon} name="ios-add-circle-outline" color="#51BBE5" size={22} />
+            <Text style={styles.findViewText}>收藏</Text>
+        </View>
+
+        <View style={[styles.findView,styles.findMT]}>
+            <Icon style={styles.findViewIcon} name="ios-images-outline" color="#51BBE5" size={22} />
+            <Text style={styles.findViewText}>相册</Text>
+        </View>
+
+        <View style={[styles.findView,styles.findMT]}>
+            <Icon style={styles.findViewIcon} name="ios-tablet-portrait" color="#BAEE44" size={22} />
+            <Text style={styles.findViewText}>卡包</Text>
+        </View>
+
+        <View style={[styles.findView,styles.findMT]}>
+            <Icon style={styles.findViewIcon} name="ios-sad-outline" color="#3399FF" size={22} />
+            <Text style={styles.findViewText}>表情</Text>
+        </View>
+
+        <View style={styles.findView}>
+            <Icon style={styles.findViewIcon} name="ios-settings-outline" color="#55C1E7" size={22} />
+            <Text style={styles.findViewText}>设置</Text>
+        </View>
+
     </View>
 );
 
