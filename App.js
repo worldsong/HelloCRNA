b80a1dc82faac6b98e2f8784706c603f8bb9b8e6
worldsong <2060884089@qq.com>
2018-05-19 14:47:53 +0800

    作业：修改选项卡背景颜色。

--- App.js
+++ App.js
@@ -1,31 +1,89 @@
 import React from 'react';
-import { View, Text,Button } from 'react-native';
-import { createBottomTabNavigator } from 'react-navigation';
+import { FlatList, StyleSheet,View, Text,Button } from 'react-native';
+import { createMaterialTopTabNavigator } from 'react-navigation';
 
-const HomeScreen = () => (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
+const dynamicScreen = () => (
+    <View style={styles.container}>
+        <Text style={styles.news}>新闻列表</Text>
+        <FlatList
+            data={[
+                {key: 'Devin'},
+                {key: 'Jackson'},
+                {key: 'James'},
+                {key: 'Joel'},
+                {key: 'John'},
+                {key: 'Jillian'},
+                {key: 'Jimmy'},
+                {key: 'Julie'},
+            ]}
+            renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
     </View>
 );
 
-const ProfileScreen = () => (
+const findScreen = () => (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Profile Screen</Text>
     </View>
 );
 
-const RootTabs = createBottomTabNavigator({
-    Home: {
-        screen: HomeScreen,
+const myScreen = () => (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Profile Screen</Text>
+    </View>
+);
+
+const RootTabs = createMaterialTopTabNavigator({
+    dynamic: {
+        screen: dynamicScreen,
+        navigationOptions: ({navigation}) => ({
+            title: '动态',
+
+        }),
     },
-    Profile: {
-        screen: ProfileScreen,
+    find: {
+        screen: findScreen,
+        navigationOptions: ({navigation}) => ({
+            title: '发现',
+        }),
     },
+    my:{
+        screen:myScreen,
+        navigationOptions: ({navigation}) => ({
+            title: '我的'
+        })
+    }
 },{
     tabBarPosition: 'bottom',
     animationEnabled: true,
     tabBarOptions: {
-        activeTintColor: '#ffffff',
+        activeTintColor: '#28a745',
+        inactiveTintColor:'#232323',
+        style: {
+            backgroundColor: '#F2F2F2'
+        },
+        indicatorStyle: {
+            height: 0
+        }
+    }
+});
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+        color:'#232323',
+    },
+    news:{
+        fontSize:20,
+        paddingLeft:10,
+        borderBottomColor:'#dadada',
+        borderBottomWidth:1,
+        justifyContent:'center',
     }
 });
 
