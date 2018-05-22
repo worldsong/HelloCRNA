d432f17940a2cffcebbd17ad075072c2fc35fbdc
worldsong <2060884089@qq.com>
2018-05-22 22:15:42 +0800

    Passing parameters to routes.

--- App.js
+++ App.js
@@ -9,7 +9,11 @@ class HomeScreen extends React.Component {
                 <Text>Home Screen</Text>
                 <Button
                     title="Go to Details"
-                    onPress={() => this.props.navigation.navigate('Details')}
+                    /* 1. Navigate to the Details route with params  */
+                    onPress={() => this.props.navigation.navigate('Details', {
+                        itemId:86,
+                        otherParam: 'anything you want here',
+                    })}
                 />
             </View>
         );
@@ -18,12 +22,21 @@ class HomeScreen extends React.Component {
 
 class DetailsScreen extends React.Component {
     render() {
+        /* 2. Get the param, provide a fallback value if not available */
+        const { navigation } = this.props;
+        const itemId = navigation.getParam('itemId', 'NO-ID');
+        const otherParam = navigation.getParam('otherParam', 'some default value');
+
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text>Details Screen</Text>
+                <Text>itemId: {JSON.stringify(itemId)}</Text>
+                <Text>otherParam: {JSON.stringify(otherParam)}</Text>
                 <Button
                     title="Go to Details... again"
-                    onPress={() => this.props.navigation.push('Details')}
+                    onPress={() => this.props.navigation.push('Details', {
+                        itemId: Math.floor(Math.random() * 100)
+                    })}
                 />
                 <Button
                     title="Go to Home"
