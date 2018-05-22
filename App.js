020724e65588f6a7da65e7609a29865361ae855b
worldsong <2060884089@qq.com>
2018-05-22 22:01:12 +0800

    go back to the first screen in the stack with this.props.navigation.popToTop().

--- App.js
+++ App.js
@@ -33,6 +33,10 @@ class DetailsScreen extends React.Component {
                     title="Go back"
                     onPress={() => this.props.navigation.goBack()}
                 />
+                <Button
+                    title="popToTop"
+                    onPress={() => this.props.navigation.popToTop()}
+                />
             </View>
         );
     }
