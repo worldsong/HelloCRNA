ccd871c6c9c06f959e93e53f62a86a81d234a076
worldsong <2060884089@qq.com>
2018-05-25 21:00:16 +0800

    Overriding shared navigationOptions.颜色反转。

--- App.js
+++ App.js
@@ -31,12 +31,16 @@ class HomeScreen extends React.Component {
 }
 
 class DetailsScreen extends React.Component {
-    static navigationOptions = ({ navigation }) => {
-        const otherParam = navigation.getParam('otherParam', 'some default value');
-
+    static navigationOptions = ({ navigation, navigationOptions }) => {
+        const { params } = navigation.state;
         return {
-            title: otherParam
-        }
+            title: navigation.getParam('otherParam','A Nested Details Screen') ,
+            /* These values are used instead of the shared configuration! */
+            headerStyle: {
+                backgroundColor: navigationOptions.headerTintColor,
+            },
+            headerTintColor: navigationOptions.headerStyle.backgroundColor,
+        };
     };
     render() {
         /* 2. Get the param, provide a fallback value if not available */
