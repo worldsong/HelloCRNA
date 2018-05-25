15533e8b10d550c5bdc5be2e2f101334de02304e
worldsong <2060884089@qq.com>
2018-05-25 21:16:31 +0800

    Replacing the title with a custom component.headerTitle。

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, Image } from 'react-native';
 import { createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
+
+class LogoTitle extends React.Component {
+    render() {
+        return (
+            <Image
+                source={require('./spiro.png')}
+                style={{ width: 30, height: 30 }}
+            />
+        );
+    }
+}
 
 class HomeScreen extends React.Component {
     static navigationOptions = {
-        title: 'Home',
-        headerStyle: {
-            backgroundColor: '#f4511e',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-            fontWeight: 'bold',
-        },
+        // headerTitle instead of title
+        headerTitle: <LogoTitle />,
     };
     render() {
         return (
