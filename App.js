8eb0d10a41191d619e2446cc77b701a38c83e2cc
worldsong <2060884089@qq.com>
2018-05-25 22:27:35 +0800

    alignItems可以决定其子元素沿着次轴。

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
 
-export default class JustifyContentBasics extends Component {
+export default class AlignItemsBasics extends Component {
     render() {
         return (
-            // 尝试把`justifyContent`改为`center`看看
+            // 尝试把`alignItems`改为`flex-start`看看
+            // 尝试把`justifyContent`改为`flex-end`看看
             // 尝试把`flexDirection`改为`row`看看
             <View style={{
                 flex: 1,
                 flexDirection: 'column',
-                justifyContent: 'flex-end',
+                justifyContent: 'center',
+                alignItems: 'flex-start',
             }}>
                 <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
                 <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
